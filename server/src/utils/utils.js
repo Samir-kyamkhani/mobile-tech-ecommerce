a6ff4d093896a678349eb126220395093f2b7b8f
Samir Khan <samirkayamkhani.dev@gmail.com>
2026-01-21 11:48:09 +0530

    add cookies otion for production

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -2,10 +2,17 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import fs from "fs";
 
+// export const cookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: "strict",
+//   maxAge: 7 * 24 * 60 * 60 * 1000,
+// };
+
 export const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
